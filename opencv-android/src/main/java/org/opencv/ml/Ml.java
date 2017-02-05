c25145edce7f77ec8aa52f311b1845945edffc7e
Felipe Rocha <felipegr6@gmail.com>
2017-02-05 02:59:16 -0200

    Gradle project structure.

---
+++ opencv-android/src/main/java/org/opencv/ml/Ml.java
@@ -0,0 +1,8 @@
+//
+// This file is auto-generated. Please don't modify it!
+//
+package org.opencv.ml;
+
+public class Ml {
+
+}
---
+++ sample/src/main/java/org/opencv/android/sample/HelloOpenCvActivity.java
@@ -0,0 +1,77 @@
+package org.opencv.android.sample;
+
+import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions public class HelloOpenCvActivity extends Activity
+    implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "OpenCV Gradle Demo";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.hello_open_cv_layout);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override public void onResume() {
+        super.onResume();
+        HelloOpenCvActivityPermissionsDispatcher.initOpenCVWithCheck(this);
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA) public void initOpenCV() {
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_8, this, mLoaderCallback);
+    }
+
+    @Override public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null) mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    break;
+
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+}
